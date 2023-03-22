b20cdf171cef197123026dc28b2234b991971e0b
Lauren <lauren.main28@gmail.com>
2023-03-21 22:25:17 -0700

    adds existence function

---
+++ math_series/series.py
@@ -0,0 +1,6 @@
+def fibonacci():
+    """
+    :parm
+    :return:
+    """
+    pass
---
+++ tests/test_series.py
@@ -0,0 +1,7 @@
+import pytest
+from math_series.series import fibonacci
+
+#order of imports: from package_name.module_name import function_name
+
+def test_fibonacci_exists():
+    assert fibonacci
