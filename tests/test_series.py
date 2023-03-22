b1a2bb2830e9bcd210dfb92d7a4a39387c80be4f
Lauren <lauren.main28@gmail.com>
2023-03-21 23:28:31 -0700

    adds lucas function and passing tests

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,6 @@
 import pytest
 from math_series.series import fibonacci
+from math_series.series import lucas
 
 #order of imports: from package_name.module_name import function_name
 
@@ -30,3 +31,21 @@ def test_fibonacci_negative():
     actual = fibonacci(-1)
     expected = None
     assert actual == expected, f"Fibonacci of a negative number should return {expected}, but got {actual} instead."
+
+def test_lucas_exists():
+    assert lucas
+
+def test_lucas_1():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_5():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
+
+def test_lucas_10():
+    actual = lucas(10)
+    expected = 123
+    assert actual == expected
